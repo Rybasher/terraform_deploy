6a6767203cfef43173ac5e452cd5b358d590ea09
michaelsizonenko <michael@steakwallet.fi>
2022-03-14 18:56:56 +0200

    test terraform

--- api-terraform/backend/main.tf
+++ api-terraform/backend/main.tf
@@ -179,43 +179,43 @@ resource "aws_lb_listener" "listener" {
   }
 }
 #
-#resource "aws_ecs_service" "my_first_service" {
-#  name            = "my-first-service"                             # Naming our first service
-#  cluster         = "${aws_ecs_cluster.test-cluster.id}"             # Referencing our created Cluster
-#  task_definition = "${aws_ecs_task_definition.my_first_task.arn}" # Referencing the task our service will spin up
-#  launch_type     = "FARGATE"
-#  desired_count   = 3 # Setting the number of containers to 3
-#
-#  load_balancer {
-#    target_group_arn = "${aws_lb_target_group.target_group.arn}" # Referencing our target group
-#    container_name   = "${aws_ecs_task_definition.my_first_task.family}"
-#    container_port   = 3000 # Specifying the container port
-#  }
-#
-#  network_configuration {
-#    subnets          = ["${aws_default_subnet.default_subnet_a.id}", "${aws_default_subnet.default_subnet_b.id}", "${aws_default_subnet.default_subnet_c.id}"]
-#    assign_public_ip = true                                                # Providing our containers with public IPs
-#    security_groups  = ["${aws_security_group.service_security_group.id}"] # Setting the security group
-#  }
-#}
-#
-#
-#resource "aws_security_group" "service_security_group" {
-#  ingress {
-#    from_port = 0
-#    to_port   = 0
-#    protocol  = "-1"
-#    # Only allowing traffic in from the load balancer security group
-#    security_groups = ["${aws_security_group.load_balancer_security_group.id}"]
-#  }
-#
-#  egress {
-#    from_port   = 0
-#    to_port     = 0
-#    protocol    = "-1"
-#    cidr_blocks = ["0.0.0.0/0"]
-#  }
-#}
+resource "aws_ecs_service" "my_first_service" {
+  name            = "my-first-service"                             # Naming our first service
+  cluster         = "${aws_ecs_cluster.test-cluster.id}"             # Referencing our created Cluster
+  task_definition = "${aws_ecs_task_definition.my_first_task.arn}" # Referencing the task our service will spin up
+  launch_type     = "FARGATE"
+  desired_count   = 3 # Setting the number of containers to 3
+
+  load_balancer {
+    target_group_arn = "${aws_lb_target_group.target_group.arn}" # Referencing our target group
+    container_name   = "${aws_ecs_task_definition.my_first_task.family}"
+    container_port   = 3000 # Specifying the container port
+  }
+
+  network_configuration {
+    subnets          = ["${aws_default_subnet.default_subnet_a.id}", "${aws_default_subnet.default_subnet_b.id}", "${aws_default_subnet.default_subnet_c.id}"]
+    assign_public_ip = true                                                # Providing our containers with public IPs
+    security_groups  = ["${aws_security_group.service_security_group.id}"] # Setting the security group
+  }
+}
+
+
+resource "aws_security_group" "service_security_group" {
+  ingress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    # Only allowing traffic in from the load balancer security group
+    security_groups = ["${aws_security_group.load_balancer_security_group.id}"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 #
 #resource "aws_db_instance" "default" {
 #  allocated_storage    = 10
