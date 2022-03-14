70d57ae4a663a080d7badf813d60ca67c9fcabf1
michaelsizonenko <michael@steakwallet.fi>
2022-03-14 18:50:56 +0200

    test terraform

--- api-terraform/backend/main.tf
+++ api-terraform/backend/main.tf
@@ -132,52 +132,52 @@ resource "aws_ecs_task_definition" "my_first_task" {
   execution_role_arn       = "arn:aws:iam::166952552828:role/ecsTaskExecutionRole"
 }
 
-#resource "aws_alb" "application_load_balancer" {
-#  name               = "test-lb-tf" # Naming our load balancer
-#  load_balancer_type = "application"
-#  subnets = [ # Referencing the default subnets
-#    "${aws_default_subnet.default_subnet_a.id}",
-#    "${aws_default_subnet.default_subnet_b.id}",
-#    "${aws_default_subnet.default_subnet_c.id}"
-#  ]
-#  # Referencing the security group
-#  security_groups = ["${aws_security_group.load_balancer_security_group.id}"]
-#}
+resource "aws_alb" "application_load_balancer" {
+  name               = "test-lb-tf" # Naming our load balancer
+  load_balancer_type = "application"
+  subnets = [ # Referencing the default subnets
+    "${aws_default_subnet.default_subnet_a.id}",
+    "${aws_default_subnet.default_subnet_b.id}",
+    "${aws_default_subnet.default_subnet_c.id}"
+  ]
+  # Referencing the security group
+  security_groups = ["${aws_security_group.load_balancer_security_group.id}"]
+}
 #
-## Creating a security group for the load balancer:
-#resource "aws_security_group" "load_balancer_security_group" {
-#  ingress {
-#    from_port   = 80
-#    to_port     = 80
-#    protocol    = "tcp"
-#    cidr_blocks = ["0.0.0.0/0"] # Allowing traffic in from all sources
-#  }
+# Creating a security group for the load balancer:
+resource "aws_security_group" "load_balancer_security_group" {
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allowing traffic in from all sources
+  }
 #
-#  egress {
-#    from_port   = 0
-#    to_port     = 0
-#    protocol    = "-1"
-#    cidr_blocks = ["0.0.0.0/0"]
-#  }
-#}
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_lb_target_group" "target_group" {
+  name        = "target-group"
+  port        = 80
+  protocol    = "HTTP"
+  target_type = "ip"
+  vpc_id      = "${aws_default_vpc.default_vpc.id}" # Referencing the default VPC
+}
 #
-#resource "aws_lb_target_group" "target_group" {
-#  name        = "target-group"
-#  port        = 80
-#  protocol    = "HTTP"
-#  target_type = "ip"
-#  vpc_id      = "${aws_default_vpc.default_vpc.id}" # Referencing the default VPC
-#}
-#
-#resource "aws_lb_listener" "listener" {
-#  load_balancer_arn = "${aws_alb.application_load_balancer.arn}" # Referencing our load balancer
-#  port              = "80"
-#  protocol          = "HTTP"
-#  default_action {
-#    type             = "forward"
-#    target_group_arn = "${aws_lb_target_group.target_group.arn}" # Referencing our tagrte group
-#  }
-#}
+resource "aws_lb_listener" "listener" {
+  load_balancer_arn = "${aws_alb.application_load_balancer.arn}" # Referencing our load balancer
+  port              = "80"
+  protocol          = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = "${aws_lb_target_group.target_group.arn}" # Referencing our tagrte group
+  }
+}
 #
 #resource "aws_ecs_service" "my_first_service" {
 #  name            = "my-first-service"                             # Naming our first service
