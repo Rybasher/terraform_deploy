225d64c98e647cbdf49ee207950428c262e9347c
michaelsizonenko <michael@steakwallet.fi>
2022-03-14 19:03:21 +0200

    test terraform

--- api-terraform/backend/main.tf
+++ api-terraform/backend/main.tf
@@ -106,12 +106,12 @@ resource "aws_ecs_cluster" "test-cluster" {
 }
 
 
-resource "aws_ecs_task_definition" "my_first_task" {
+resource "aws_ecs_task_definition" "indexer-task" {
   family                   = "indexer-task" # Naming our first task
   container_definitions    = <<DEFINITION
   [
     {
-      "name": "my-first-task",
+      "name": "indexer-task",
       "image": "${aws_ecr_repository.indexer-repo.repository_url}",
       "essential": true,
       "portMappings": [
@@ -182,13 +182,13 @@ resource "aws_lb_listener" "listener" {
 resource "aws_ecs_service" "my_first_service" {
   name            = "my-first-service"                             # Naming our first service
   cluster         = "${aws_ecs_cluster.test-cluster.id}"             # Referencing our created Cluster
-  task_definition = "${aws_ecs_task_definition.my_first_task.arn}" # Referencing the task our service will spin up
+  task_definition = "${aws_ecs_task_definition.indexer-task.arn}" # Referencing the task our service will spin up
   launch_type     = "FARGATE"
   desired_count   = 3 # Setting the number of containers to 3
 
   load_balancer {
     target_group_arn = "${aws_lb_target_group.target_group.arn}" # Referencing our target group
-    container_name   = "${aws_ecs_task_definition.my_first_task.family}"
+    container_name   = "${aws_ecs_task_definition.indexer-task.family}"
     container_port   = 3000 # Specifying the container port
   }
 
