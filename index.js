2fe485bfd160b5449b385d647b97c0ac48884c2b
michaelsizonenko <michael@steakwallet.fi>
2022-06-27 14:37:01 +0300

    blue green test

--- index.js
+++ index.js
@@ -3,5 +3,8 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => res.send('Hello World!!!!!!222!!!!!!!!'))
+app.get('/error', (req, res) => {
+    process.exit(1)
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
